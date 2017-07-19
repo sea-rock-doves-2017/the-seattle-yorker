b46e0ea6d70417c80e2ba25ffc1137fdb57d9bdb
Grace Noh <email": null>
2017-07-19 12:27:08 -0700

    add tests for article index controller

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe ArticlesController, type: :controller do
+  let!(:article) { Article.create(title: 'Hamz All da Way', body: 'hamz hamz hamz times 50', author: 'Sir Hamzalot', category: 'for funsies') }
 
+  describe 'GET #index' do
+    before(:each) { get :index }
 
+    it 'responds with status code 200' do
+      # expect(response.status).to eq 200
+    end
 
+    it 'assigns the @articles variable with a value' do
+      # expect(assigns[:articles]).to eq (Article.all)
+    end
+
+  end
 end
