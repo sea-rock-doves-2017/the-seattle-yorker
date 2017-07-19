b4c1b6bc955ba1f3005194d75247724d630d3ee4
Grace Noh <email": null>
2017-07-19 12:16:18 -0700

    pass tests for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,12 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:user) { User.create(username: 'sir_hamsalot', password_digest: 'password') }
 
-  it 'has a username'
+  it 'has a username' do
+    expect(user.username).to eq 'sir_hamsalot'
+  end
 
-  it 'has a password'
+  it 'has a password' do
+    expect(user.password_digest).to eq 'password'
+  end
 
 end
