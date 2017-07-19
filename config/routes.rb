4d5cce142e22e6c1f05321b97621287dacdf3516
Grace Noh <email": null>
2017-07-19 11:41:55 -0700

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get 'login' => 'sessions#new'
+
+  root to: 'articles#index'
+
 end
