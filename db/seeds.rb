629c2d88d80f6191b2e298d5bc404e08b0a5bf54
Grace Noh <email": null>
2017-07-19 11:20:04 -0700

    add models and migrates; create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+3.times do
+  User.create(username: Faker::Internet.user_name, password: password)
+end
+
+
+50.times do
+  categories = ['news', 'culture', 'books', 'business & tech', 'humor', 'cartoons']
+  assigned_category = categories[rand(0..5)]
+  Article.create(title: Faker::Witcher.quote, body: Faker::Hipster.paragraphs(3), author: Faker::Name.name, category: assigned_category)
+end
