2f02751bf1a4311325bf22de345b6e6076cb701a
Grace Noh <email": null>
2017-07-19 12:14:44 -0700

    pass tests for Article

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,14 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  let(:article) { Article.create(title: 'Sample', body: 'body here', category: 'for funsies') }
+  let(:article) { Article.create(title: 'Hamz All da Way', body: 'hamz hamz hamz times 50', author: 'Sir Hamzalot', category: 'for funsies') }
 
-  it 'has a title'
+  it 'has a title' do
+    expect(article.title).to eq 'Hamz All da Way'
+  end
 
-  it 'has a body'
+  it 'has a body' do
+    expect(article.body).to eq 'hamz hamz hamz times 50'
+  end
 
-  it 'has an author'
+  it 'has an author' do
+    expect(article.author).to eq 'Sir Hamzalot'
+  end
 
-  it 'has a category'
+  it 'has a category' do
+    expect(article.category).to eq 'for funsies'
+  end
 
 end
